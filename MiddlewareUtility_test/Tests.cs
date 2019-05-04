65906670ab3003ce2c87e6a9b30606782a52065e
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-05-04 21:55:34 +0800

    Initial rep

---
+++ MiddlewareUtility_test/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MiddlewareUtility_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
