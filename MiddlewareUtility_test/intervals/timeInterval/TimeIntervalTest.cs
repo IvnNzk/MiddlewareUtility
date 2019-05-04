f9b029071cb109ea2d1319ee04218a6a5b2d2229
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-05-04 23:47:38 +0800

    On branch master 	Add blank test for TimeInterval class.
     Changes  committed:
    	modified:   MiddlewareUtility_test/MiddlewareUtility_test.csproj
    	new file:   MiddlewareUtility_test/intervals/timeInterval/TimeIntervalTest.cs

---
+++ MiddlewareUtility_test/intervals/timeInterval/TimeIntervalTest.cs
@@ -0,0 +1,15 @@
+namespace MiddlewareUtility_test.intervals.timeInterval
+{
+    using System;
+    using MiddlewareUtility.intervals.timeInterval.implementation;
+    using NUnit.Framework;
+
+    public class TimeIntervalTest
+    {
+        [Test]
+        public void DayIntervalEmptyConstructor_ShouldReturnException()
+        {
+            Assert.Catch<ArgumentException>(() => new TimeInterval());
+        }
+    }
+}
