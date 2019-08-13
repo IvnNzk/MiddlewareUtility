23978f61956a5e62645e559785972e5c5cc768bb
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-08-14 00:58:58 +0800

    Change Naming in constructor test for DayInterval

--- MiddlewareUtility_test/Intervals/DayInterval/DayIntervalTests.cs
+++ MiddlewareUtility_test/Intervals/DayInterval/DayIntervalTests.cs
@@ -8,46 +8,46 @@ namespace MiddlewareUtility_test.intervals.DayInterval
 
     public class DayIntervalTest
     {
+        // Both DateTime in Utc
         [Test]
-        public void CreateInstanceDayIntervalByDateTimeOffset()
+        public void Constructor_DataTime_BothUtc_KindMustUtc()
         {
-            var dayInterval = new DayInterval(DateTime.Now, DateTime.Now);
-            Assert.IsNotNull(dayInterval);
+            throw new NotImplementedException();
+        }
+
+        // Both DateTime in Local
+        [Test]
+        public void Constructor_DataTime_BothLocal_KindMustLocal()
+        {
+            throw new NotImplementedException();
+        }
+
+        //DateTime in Unspecific
+        [Test]
+        public void Constructor_DataTime_BothLocal_ReturnExceptionIllegalArgument()
+        {
+            throw new NotImplementedException();
         }
 
         // DateTime in Utc and Local format
         [Test]
-        public void CreateDayIntervalWithDifferentDataTime_Utc_Local_ReturnExceptionIllegalArgument()
+        public void Constructor_WithDifferentDataTime_Utc_Local_ReturnExceptionIllegalArgument()
         {
             throw new NotImplementedException();
         }
 
         // DateTime in Utc and Unspecific format
         [Test]
-        public void CreateDayIntervalWithDifferentDataTime_Utc_Unspecific_ReturnExceptionIllegalArgument()
+        public void Constructor_WithDifferentDataTime_Utc_Unspecific_ReturnExceptionIllegalArgument()
         {
             throw new NotImplementedException();
         }
 
         // DateTime in Local and Unspecific format
         [Test]
-        public void CreateDayIntervalWithDifferentDataTime_Local_Unspecific_ReturnExceptionIllegalArgument()
+        public void Constructor_WithDifferentDataTime_Local_Unspecific_ReturnExceptionIllegalArgument()
         {
-            throw new NotImplementedException();
-        }
-        
-        // DateTime in Utc
-        [Test]
-        public void CreateDayIntervalWithDifferentDataTime_BothUtc_KindMustUtc()
-        {
-            throw new NotImplementedException();
-        }
-
-        // DateTime in Local
-        [Test]
-        public void CreateDayIntervalWithDifferentDataTime_BothLocal_KindMustLocal()
-        {
-            throw new NotImplementedException();
+            throw  new NotImplementedException();
         }
     }
 }
