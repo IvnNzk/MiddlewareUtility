7ade065f8f8074085d999828087e4c064cfd706f
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-08-20 23:22:20 +0800

    Add empty test for TableRow

---
+++ MiddlewareUtility_test/CalibrationTable/TableRow/TableRowTest.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MiddlewareUtility_test.CalibrationTable.TableRow
+{
+    using System.CodeDom;
+    using NUnit.Framework;
+
+    public class TableRowTest
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            throw new NotImplementedException("confines?");
+        }
+    }
+}
