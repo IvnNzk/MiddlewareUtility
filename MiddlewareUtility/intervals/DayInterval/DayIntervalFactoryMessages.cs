2658c2e7cf977d662473eeb4128cb122cf2e3113
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-08-20 22:47:44 +0800

    add messages for DayIntervalFactory

---
+++ MiddlewareUtility/intervals/DayInterval/DayIntervalFactoryMessages.cs
@@ -0,0 +1,9 @@
+namespace MiddlewareUtility.Tools
+{
+    public static class DayIntervalFactoryMessages
+    {
+        public static string TIME_IN_UNSPECIFIED { get; } = "Time doesn't be unspecified.";
+        public static string OFFSET_MORE_THEN_MAXVALUE { get; } = "Offset more than maxvalue";
+        public static string OFFSET_LESS_THEN_MINVALUE { get; } = "Offset less than minvalue";
+    }
+}
