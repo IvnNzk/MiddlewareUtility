f080ef7e5c196f4e72e8c8536f2ea212fe1f4a14
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2019-08-26 23:40:48 +0800

    emptyTestFor CalibrationTable. GetValue and GetInterpolatedValue testsMethods.

--- MiddlewareUtility_test/CalibrationTable/CalibrationTable/CalibrationTableTests.cs
+++ MiddlewareUtility_test/CalibrationTable/CalibrationTable/CalibrationTableTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security.Permissions;
 
 namespace MiddlewareUtility_test.CalibrationTable.CalibrationTable
@@ -9,7 +10,7 @@ namespace MiddlewareUtility_test.CalibrationTable.CalibrationTable
     public class CalibrationTableTests
     {
         private CalibrationTable<TableRow> _table;
-        
+
         private TableRow _leftBoundValue;
         private TableRow _rightBoundValue;
 
@@ -24,7 +25,7 @@ namespace MiddlewareUtility_test.CalibrationTable.CalibrationTable
 
             for (var i = 0; i < 10; i++)
             {
-                var row = new TableRow(i, curValue);
+                var row = new TableRow(i * 100, curValue);
                 rowsList.Add(row);
 
                 if (i == 0)
@@ -44,7 +45,7 @@ namespace MiddlewareUtility_test.CalibrationTable.CalibrationTable
             var rowsList = new List<TableRow>();
             rowsList.Add(new TableRow(0, 0));
             rowsList.Add(new TableRow(1, 1.2));
-            rowsList.Add(new TableRow(1, 4.2));
+            rowsList.Add(new TableRow(2, 4.2));
             var table = new CalibrationTable<TableRow>(rowsList);
 
             Assert.IsNotNull(table);
@@ -61,11 +62,85 @@ namespace MiddlewareUtility_test.CalibrationTable.CalibrationTable
         {
             Assert.AreEqual(_leftBoundValue, _table.LeftBoundValue);
         }
-        
+
         [Test]
         public void PropertyRightBoundValueTest()
         {
             Assert.AreEqual(_rightBoundValue, _table.RightBoundValue);
+        }
+
+
+        /*
+         * GetInterpolatedValue tests
+         */
+        [Test]
+        public void GetInterpolatedValueTest()
+        {
+            throw new NotImplementedException("How to check returned values?");
+        }
+
+        [Test]
+        public void GetInterpolatedValueTest_IndexMoreThanTableIndex_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GetInterpolatedValueTest_IndexLessThanTableIndex_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         * GetValue Test
+         */
+        [Test]
+        public void GetValueTest_modeAtOrBefore_checkReturnedValues()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void GetValueTest_modeAtOrAfter_checkReturnedValues()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void GetValueTest_modeBefore_checkReturnedValues()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void GetValueTest_modeAfter_checkReturnedValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GetValueTest_modeAtOrBefore_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void GetValueTest_modeBefore_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GetValueTest_modeAtOrAfter_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GetValueTest_modeAfter_returnArgumentOutOfRangeException()
+        {
+            throw new NotImplementedException();
         }
     }
 }
